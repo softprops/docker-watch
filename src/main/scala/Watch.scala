d2e7b43d3e019bc8b3939a7baf9513e02f4b0755
softprops <d.tangren@gmail.com>
2014-10-21 01:04:55 -0400

    a filter interface

--- src/main/scala/Watch.scala
+++ src/main/scala/Watch.scala
@@ -2,8 +2,28 @@ package dockerwatch
 
 import tugboat.{ Docker, Event }
 
-case class Watch
- (docker: Docker)
- (f: Event.Record => Unit) {
-  def start = docker.events.stream(f)
+object Watch {
+  trait Stop {
+    def stop(): Unit
+  }
+}
+
+case class Watch(docker: Docker) {
+  protected class WithFilter(filter: Event.Record => Boolean)
+    extends Watch(docker) {
+    override def apply(f: Event.Record => Unit): Watch.Stop =
+      new Watch.Stop {
+        val (s, _) = docker.events.stream { record =>
+          if (filter(record)) f(record)
+        }
+        def stop() = s.stop()
+      }
+  }
+
+  def filter(filt: Event.Record => Boolean): Watch = new WithFilter(filt)
+  def apply(f: Event.Record => Unit): Watch.Stop =
+    new Watch.Stop {
+      val (s, _) = docker.events.stream(f)
+      def stop() = s.stop()
+    }
 }
